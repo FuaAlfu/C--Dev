e70c802e40a38fe7cd9618ff58a0e9ec034847af
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-06-21 15:25:26 +0300

    add

---
+++ 003-syntax/005-ListConstructorAndToList/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 2021.6.21
+/// </summary>
+
+/*
+TODO
+*/
+
+namespace _005_ListConstructorAndToList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create a string array.
+            string[] array = new string[]
+            {
+            "apple",
+            "orange",
+            "banana",
+            };
+
+            //list constructor.
+            List<string> list1 = new List<string>(array);
+            Testing(list1);
+
+            //here ..ToList method.
+            List<string> list2 = array.ToList();
+            Testing(list2);
+        }
+
+        static void Testing(List<string> list)
+        {
+            Console.WriteLine("List count: " + list.Count);
+        }
+    }
+}
