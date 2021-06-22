b0cf8d2580cc6f728fd44f33afc5836c0a5dbdfb
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-06-23 02:11:20 +0300

    add

---
+++ 003-syntax/006-copies-array-section/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _006_copies_array_section
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // an integer array.
+        int[] myArray = new int[]{10,20,30,40,50,60,70,80,90,100};
+
+        // Instantiate the target.
+        int[] t = new int[3];
+
+        // Copy first 3 elements in myArray to target.
+        Array.Copy(myArray, 0, t, 0, 3);
+
+        // ranging over the target.
+        foreach (int value in t)
+        {
+            Console.WriteLine(value);
+        }
+        Console.WriteLine("---");
+
+        //print the length of myArray
+        Console.WriteLine("the length of myArray: " + myArray.Length);
+        }
+    }
+}
