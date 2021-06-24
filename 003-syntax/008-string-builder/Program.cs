b4d6aa9d11f9defca30c63981c8ca32cdf9be7a4
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-06-24 16:27:18 +0300

    add

---
+++ 003-syntax/008-string-builder/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+/// <summary>
+/// 2021.6.24
+/// </summary>
+
+/*
+TODO
+*/
+
+namespace _008_string_builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare capacity by 60  
+            StringBuilder myString = new StringBuilder("you are awesome ", 60);
+
+            //append a new line
+            myString.Append("dude.. ");
+
+            //append another new line 
+            myString.AppendLine("hell yeah..");
+
+            myString.Append("duuuudes ...");
+            Console.WriteLine(myString);
+        }
+    }
+}
---
+++ 003-syntax/008-string-builder/readme.md
@@ -0,0 +1,10 @@
+---
+stack: C#
+lang: all
+---
+
+## StringBuilder in C#
+*C# StringBuilder* is similar to Java StringBuilder.
+A String object is immutable,
+a String cannot be changed once created.
+Every time when you use any of the methods of the System.String class, then you create a new string object in memory.
