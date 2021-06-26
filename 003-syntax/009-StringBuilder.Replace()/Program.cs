ae7cefbafd7ae1117ce0ecbb494e163a3f43a81f
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-06-26 20:57:38 +0300

    add

---
+++ 003-syntax/009-StringBuilder.Replace()/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+/// <summary>
+/// 2021.6.26
+/// </summary>
+
+/*
+TODO
+*/
+namespace _009_StringBuilder.Replace__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare capacity by 20
+        StringBuilder myString = new StringBuilder("### is a Russian signer ", 20);
+          
+        //replace "###" with "yuta"
+        myString.Replace("###", "yuta");
+  
+        Console.WriteLine(myString);
+        }
+    }
+}
---
+++ 003-syntax/009-StringBuilder.Replace()/readme.md
@@ -0,0 +1,7 @@
+---
+stack: C#
+lang: all
+---
+
+## StringBuilder in C# :: StringBuilder.Replace(old_val, new_val) Method
+This method is used to replace characters within the StringBuilder object with another specified character.
