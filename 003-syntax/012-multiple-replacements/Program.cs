d2c1d21dc74e506d712b684b3a5b149a4dfa9c89
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-07-07 20:14:17 +0300

    add

---
+++ 003-syntax/012-multiple-replacements/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _012_multiple_replacements
+{
+    class Program
+    {
+        const string MY_VALUE = "abcabc";
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("MY_VALUE Before: " + MY_VALUE);
+
+            /*
+             Store the result of Replace() in a variable.
+             and then all instances of the substring are replaced.
+             */
+            string modified = MY_VALUE.Replace("bc", "?");
+            Console.WriteLine("MY_VALUE After:  " + modified);
+        }
+    }
+}
---
+++ 003-syntax/012-multiple-replacements/readme.md
@@ -0,0 +1,8 @@
+---
+stack: C#
+lang: all
+---
+
+## multiple replacements
+The Replace method changes every instance of the specified substring.
+This can sometimes result in unexpected behavior—we must be careful.
