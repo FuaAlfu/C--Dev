e9b3da6c7e4a182b050c13b8695a31813070fcca
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-07-08 21:15:29 +0300

    add

---
+++ 003-syntax/013-Regex.Replace/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+/// <summary>
+/// 2021.7.8
+/// </summary>
+
+namespace _013_Regex.Replace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value = "bird0 cat1 bird2 cat2 bird.";
+            Console.WriteLine("Before: " + value);
+
+            // Match all strings starting with "bird" and ending with a digit and non-word character.
+            //Replace them with an empty string literal.
+            string result = Regex.Replace(value, @"bird\d\W", "");
+            Console.WriteLine("After:  " + result);
+        }
+    }
+}
---
+++ 003-syntax/013-Regex.Replace/readme.md
@@ -0,0 +1,8 @@
+---
+stack: C#
+lang: all
+---
+
+## Regex
+The string.Replace() method is limited in the kinds of substrings it can match.
+With Regex we can match met a-characters like "digit" or "non-word."
