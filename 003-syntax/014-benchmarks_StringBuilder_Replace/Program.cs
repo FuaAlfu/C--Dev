841d0d816f1c723cc223e26f9e04d633f39c0890
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-07-09 20:37:11 +0300

    add

---
+++ 003-syntax/014-benchmarks_StringBuilder_Replace/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+/// <summary>
+/// 2021.7.9
+/// </summary>
+
+namespace _014_benchmarks_StringBuilder_Replace
+{
+    const int MAX_SIZE = 1000000;
+    static void Main()
+    {
+        var text = "bird, frog and dog";
+        var s1 = Stopwatch.StartNew();
+        //use string Replace.
+        for (int i = 0; i < MAX_SIZE; i++)
+        {
+            string result = text;
+            result = result.Replace("bird", "cat");
+            result = result.Replace("frog", "fox");
+            result = result.Replace("dog", "fish");
+        }
+        s1.Stop();
+        var s2 = Stopwatch.StartNew();
+        //use StringBuilder.
+        for (int i = 0; i < MAX_SIZE; i++)
+        {
+            var builder = new StringBuilder(text);
+            builder.Replace("bird", "cat");
+            builder.Replace("frog", "fox");
+            builder.Replace("dog", "fish");
+        }
+        s2.Stop();
+        Console.WriteLine(((double)(s1.Elapsed.TotalMilliseconds * 1000000) / MAX_SIZE).ToString("0.00 ns"));
+        Console.WriteLine(((double)(s2.Elapsed.TotalMilliseconds * 1000000) / MAX_SIZE).ToString("0.00 ns"));
+    }
+}
---
+++ 003-syntax/014-benchmarks_StringBuilder_Replace/readme.md
@@ -0,0 +1,8 @@
+---
+stack: C#
+lang: all
+---
+
+## benchmarks StringBuilder Replace
+Sometimes StringBuilder can lead to performance improvements.
+The string type in C# can be faster in many situations.
