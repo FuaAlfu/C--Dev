be1bc8c1bcde6b4f529d186792c50443ef75ad66
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-08-02 10:55:12 +0300

    add

---
+++ 003-syntax/015-array-revers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _015_array_revers
+{
+    /*
+    notes..
+    */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        int[] array = { 1, 2, 3 };
+
+        foreach (int value in array)
+        {
+            Console.WriteLine(value);
+        }
+        Console.WriteLine();
+        
+        //Reverse method
+        Array.Reverse(array);
+        
+        //range over array
+        foreach (int value in array)
+        {
+            Console.WriteLine(value);
+        }
+        Console.WriteLine();
+        
+        //Reverse again :: return
+        Array.Reverse(array);
+        
+        foreach (int value in array)
+        {
+            Console.WriteLine(value);
+        }
+        }
+    }
+}
---
+++ 003-syntax/015-array-revers/readme.md
@@ -0,0 +1,10 @@
+---
+stack: C#
+lang: all
+---
+
+## How does the Array.Reverse method work
+the method checks the parameter and then invokes TrySZReverse: this particular method is implemented in native code, not managed code.
+
+---
+---
