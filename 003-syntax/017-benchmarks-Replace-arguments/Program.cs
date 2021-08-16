4714b28634fd0ddc3d188ae5108b633a9650f9a9
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-08-16 19:24:28 +0300

    add

---
+++ 003-syntax/017-benchmarks-Replace-arguments/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace _017_benchmarks_Replace_arguments
+{
+    class Program
+    {
+        const int Max_Num = 1000000;
+        static void Main()
+        {
+            string test = "cat";
+            var s1 = Stopwatch.StartNew();
+            // Version 1: use char argument.
+            for (int i = 0; i < Max_Num; i++)
+            {
+                string result = test.Replace('c', 'z');
+            }
+            s1.Stop();
+            var s2 = Stopwatch.StartNew();
+            // Version 2: use string argument.
+            for (int i = 0; i < Max_Num; i++)
+            {
+                string result = test.Replace("c", "z");
+            }
+            s2.Stop();
+            Console.WriteLine(((double)(s1.Elapsed.TotalMilliseconds * 1000000) / Max_Num).ToString("0.00 ns"));
+            Console.WriteLine(((double)(s2.Elapsed.TotalMilliseconds * 1000000) / Max_Num).ToString("0.00 ns"));
+        }
+    }
+}
---
+++ 003-syntax/017-benchmarks-Replace-arguments/readme.md
@@ -0,0 +1,8 @@
+---
+stack: C#
+lang: all
+---
+
+## Benchmark, chars...
+Sometimes, we can use char arguments instead of 1-char strings.
+We test a program that calls Replace with different kinds of arguments.
