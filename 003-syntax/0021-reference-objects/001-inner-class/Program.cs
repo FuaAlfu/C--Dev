8313f75c1d9045994e77515bcefa1cb06e9cb0a6
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-09-06 01:33:08 +0300

    add

---
+++ 003-syntax/0021-reference-objects/001-inner-class/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _001_inner_class
+{
+    class Program
+    {
+           static void Main(string[] args)
+            {
+                Person p = new Person();
+                p.name = "john doe";
+                p.age = 42;
+                Console.WriteLine($"his name: {p.name} and his age about: {p.age}");
+            }
+
+             //inner class
+       class Person
+       {
+         public string name;
+         public int age;
+       }
+    }
+}
---
+++ 003-syntax/020-libraries/001-system/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _001_system
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float num = 7.5f;
+            Console.WriteLine(num);
+
+            //using Math library
+            Console.WriteLine(Math.Floor(num));
+            Console.WriteLine(Math.Ceiling(num));
+            Console.WriteLine(Math.Round(num));
+        }
+        
+    }
+}
