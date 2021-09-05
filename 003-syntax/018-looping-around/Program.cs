3b44eac4d9f4e61767ff9def24f262dc85adf61a
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-09-05 10:40:03 +0300

    add

---
+++ 003-syntax/018-looping-around/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _018_looping_around
+{
+    class Program
+    {
+            static void Main(string[] args)
+            {   
+                Repeat(7);
+            }
+
+             static void Repeat(int v) 
+        {                     
+              for (int i = 0; i < v; i++)
+              {      
+               Console.WriteLine("value");
+              }
+        }
+    }
+}
+
---
+++ 003-syntax/019-more-ref-out/001-out/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _001_out
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            Console.WriteLine(x);
+            MyFunc(out x);
+            Console.WriteLine(x);
+        }
+
+        static void MyFunc(out int v)
+        {
+            v = 9;
+            Console.WriteLine(v);
+        }
+    }
+}
