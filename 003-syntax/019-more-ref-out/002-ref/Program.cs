a3c9e87c29ef37d3b6b7124cff5f0a5a1d381c0b
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-09-05 10:44:54 +0300

    add

---
+++ 003-syntax/019-more-ref-out/002-ref/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _002_ref
+{
+    class Program
+    {
+          static void Main(string[] args)
+        {
+            int x = 10;
+            Console.WriteLine(x);
+            MyDoubleFunc(ref x);
+            Console.WriteLine(x);
+        }
+
+        static void MyDoubleFunc(ref int v)
+        {
+            v = v * 2;
+            Console.WriteLine(v);
+        }
+    }
+}
