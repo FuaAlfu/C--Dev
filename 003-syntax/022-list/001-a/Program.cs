143c4e25e4691933d34ca373d17a74c16d79b35a
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-09-07 08:53:49 +0300

    add

---
+++ 003-syntax/022-list/001-a/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// 2021.9.7
+/// </summary>
+
+namespace _001_a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           List<string> users = new List<string>();
+
+           users.Add("user1");
+           users.Add("user2");
+           Console.WriteLine(users);
+
+           //Listing 
+           // Collection of string  
+           string[] usersGroup = { "user3", "user4", "user5" };    
+            
+           users.AddRange(usersGroup);  
+           //range over a list of users
+          foreach (string u in users)
+          {  
+           Console.WriteLine(u); 
+          }
+          System.Console.WriteLine("====");
+
+           //remove an element from the list
+           users.Remove("user1");
+           foreach (string u in users)
+           Console.WriteLine(u); 
+          }
+    }
+}
