d9e5633e6c109328fbcabfc04c91d9515d3c9376
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-09-08 08:40:50 +0300

    add

---
+++ 003-syntax/022-list/002-b/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _002_b
+{
+      class Program
+    {
+    static void Main(string[] args)
+    {
+        List<User> users = new List<User>()
+        {
+        new User() { Name = "John Doe", Age = 42 },
+        new User() { Name = "fua alfu", Age = 29 },
+        new User() { Name = "mua alfu", Age = 24 },
+        };
+
+        users.Sort(CompareUsers);
+        foreach (User user in users)
+        Console.WriteLine(user.Name + ": " + user.Age + " years old");
+    }
+
+    public static int CompareUsers(User user1, User user2)
+    {
+        return user1.Age.CompareTo(user2.Age);
+    }
+}
+}
---
+++ 003-syntax/022-list/002-b/User.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace _002_b
+{
+    public class User
+    {
+    public string Name { get; set; }
+    public int Age { get; set; }
+    }
+}
