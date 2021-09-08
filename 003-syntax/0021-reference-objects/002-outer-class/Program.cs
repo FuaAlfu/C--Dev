dd22ef97f3ab0d71e86a842b51694aec95facedb
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-09-08 09:25:54 +0300

    add

---
+++ 003-syntax/0021-reference-objects/002-outer-class/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _002_outer_class
+{
+    class Program
+    {
+          static void Main(string[] args)
+            {
+                Person p1 = new Person("john",42);
+                
+                Console.WriteLine($"his name: {p1.Name} and his age about: {p1.Age}");
+            }
+    }
+            //outer class
+     public class Person
+       {
+         public string Name { get; set; }
+         public int Age { get; set; }
+
+         public Person(string name, int age)
+         {
+             this.Name = name;
+             this.Age = age;
+         }
+       }
+}
