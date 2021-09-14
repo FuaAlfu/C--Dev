7142112c5a3fe2610de5c1db7722a2b6514bd592
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-09-14 15:29:02 +0300

    add

---
+++ 003-syntax/023-linkedList/001-a/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _001_a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // creating a single linkedlist by Using LinkedList class
+            LinkedList<String> myList = new LinkedList<String>();
+
+            // adding an element by using AddLast() method
+            myList.AddLast("john");
+            myList.AddLast("doe");
+            myList.AddLast("is");
+            myList.AddLast("awesome");
+
+            // Ranging over a LinkedList Using foreach loop
+            foreach (string name in myList)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
